21a8a89165b80792b5b7e4be526f22cfb6d610d6
Jonathan Rogers <jrogers07@gmail.com>
2014-05-08 11:30:30 -0600

    addedhome, contact, portfolio and resource pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  root "pages#home"
+  get "about" => "pages#contact"
+  get "about" => "pages#portfolio"
+  get "about" => "pages#resources"
+
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
